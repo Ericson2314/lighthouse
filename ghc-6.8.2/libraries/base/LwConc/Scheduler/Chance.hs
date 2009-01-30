0867bbe4b1f8cf481b84a5c1db6f6eeabbbcdb84
Kenny Graunke <kennyg@cs.pdx.edu>
2009-01-30 13:09:59 -0800

    Add Chance and FixedHigh schedulers
    Ignore-this: d384fdf263354a74aaf76fb554e2d798
    
    darcs-hash:20090130210959-f2e3e-ccb48c3b716932b12c9ba480b0327e2a3ec899bf.gz

---
+++ ghc-6.8.2/libraries/base/LwConc/Scheduler/Chance.hs
@@ -0,0 +1,83 @@
+module LwConc.Scheduler.Chance
+( getNextThread
+, schedule
+, timeUp
+, dumpQueueLengths
+) where
+
+-- This is a multi-level queue scheduler, taking priority into account.
+--
+-- To select a priority level, it starts with maxBound, and rolls a die.
+-- If d100 < 10, it moves to a lower level, and starts over.
+
+import System.IO.Unsafe (unsafePerformIO)
+import Data.Sequence as Seq
+import GHC.Arr as Array
+import LwConc.Priority
+import LwConc.PTM
+import LwConc.Substrate
+
+import GHC.Float(double2Int, int2Double)
+
+-- Introducing the world's worst random number generator (32-bit only)
+-- http://en.wikipedia.org/wiki/Linear_congruential_generator
+randomX :: PVar Integer
+randomX = unsafePerformIO $ newPVarIO 42133786 -- just some seed
+
+random :: PTM Int
+random = do x <- readPVar randomX
+            let x' = (1103515245 * x + 12345) `mod` 4294967296
+            writePVar randomX x'
+            return (fromInteger x')
+
+-- |Returns a random number between 0 and 99...probably.
+d100 :: PTM Int
+d100 = do x <- random
+          return $ double2Int ((int2Double x) * 100 / 4294967296)
+
+timeUp :: IO Bool
+timeUp = return True
+
+type ReadyQ = PVar (Seq SCont)
+
+
+-- |An array of ready queues, indexed by priority.
+readyQs :: Array Priority ReadyQ
+readyQs = listArray (minBound, maxBound) (unsafePerformIO $ sequence [newPVarIO Seq.empty | p <- [minBound .. maxBound :: Priority]])
+
+-- |Returns which priority to pull the next thread from, and updates the countdown for next time.
+getNextPriority :: PTM Priority
+getNextPriority = helper maxBound
+  where helper x | x == minBound = return x
+        helper x = do r <- d100
+                      if r < 10 -- % chance of trying a lower priority.
+                         then helper (pred x)
+                         else return x
+
+-- |Returns the next ready thread, or Nothing.
+getNextThread :: PTM (Maybe SCont)
+getNextThread = do priority <- getNextPriority
+                   tryAt priority
+  where tryAt priority = do let readyQ = readyQs ! priority
+                            q <- readPVar readyQ
+                            case viewl q of
+                              (t :< ts) -> do writePVar readyQ ts
+                                              return (Just t)
+                              EmptyL -> if priority == minBound
+                                           then return Nothing
+                                           else tryAt (pred priority) -- nothing to run at this priority, try something lower.
+
+-- |Marks a thread "ready" and schedules it for some future time.
+schedule :: SCont -> PTM ()
+schedule thread =
+  do priority <- unsafeIOToPTM myPriority
+     let readyQ = readyQs ! priority
+     q <- readPVar readyQ
+     writePVar readyQ (q |> thread)
+
+dumpQueueLengths :: (String -> IO ()) -> IO ()
+dumpQueueLengths cPrint = mapM_ dumpQL [minBound .. maxBound]
+  where dumpQL :: Priority -> IO ()
+        dumpQL p = do len <- atomically $ do q <- readPVar (readyQs ! p)
+                                             return (Seq.length q)
+                      cPrint ("|readyQ[" ++ show p ++ "]| = " ++ show len ++ "\n")
---
+++ ghc-6.8.2/libraries/base/LwConc/Scheduler/FixedHigh.hs
@@ -0,0 +1,54 @@
+module LwConc.Scheduler.FixedHigh
+( getNextThread
+, schedule
+, timeUp
+, dumpQueueLengths
+) where
+
+-- This is a multi-level queue scheduler, taking priority into account.
+--
+-- It always chooses to run the highest priority thread available.
+-- This will likely starve other threads; it's not designed for general use.
+
+import System.IO.Unsafe (unsafePerformIO)
+import Data.Sequence as Seq
+import GHC.Arr as Array
+import LwConc.Priority
+import LwConc.PTM
+import LwConc.Substrate
+
+timeUp :: IO Bool
+timeUp = return True
+
+type ReadyQ = PVar (Seq SCont)
+
+-- |An array of ready queues, indexed by priority.
+readyQs :: Array Priority ReadyQ
+readyQs = listArray (minBound, maxBound) (unsafePerformIO $ sequence [newPVarIO Seq.empty | p <- [minBound .. maxBound :: Priority]])
+
+-- |Returns the next ready thread, or Nothing.
+getNextThread :: PTM (Maybe SCont)
+getNextThread = tryAt maxBound
+  where tryAt priority = do let readyQ = readyQs ! priority
+                            q <- readPVar readyQ
+                            case viewl q of
+                              (t :< ts) -> do writePVar readyQ ts
+                                              return (Just t)
+                              EmptyL -> if priority == minBound
+                                           then return Nothing
+                                           else tryAt (pred priority) -- nothing to run at this priority, try something lower.
+
+-- |Marks a thread "ready" and schedules it for some future time.
+schedule :: SCont -> PTM ()
+schedule thread =
+  do priority <- unsafeIOToPTM myPriority
+     let readyQ = readyQs ! priority
+     q <- readPVar readyQ
+     writePVar readyQ (q |> thread)
+
+dumpQueueLengths :: (String -> IO ()) -> IO ()
+dumpQueueLengths cPrint = mapM_ dumpQL [minBound .. maxBound]
+  where dumpQL :: Priority -> IO ()
+        dumpQL p = do len <- atomically $ do q <- readPVar (readyQs ! p)
+                                             return (Seq.length q)
+                      cPrint ("|readyQ[" ++ show p ++ "]| = " ++ show len ++ "\n")
