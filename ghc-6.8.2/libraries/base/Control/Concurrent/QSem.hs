ad4ae113f1ac9e6cea000fd86cce7edbdfb776a9
Kenny Graunke <kennyg@cs.pdx.edu>
2009-02-02 15:17:09 -0800

    Reimplement QSem atop PTM (multiple wakeup) to prevent large scale priority inversion
    Ignore-this: 754ca009c9d4d98e5535755d774b33ab
    
    darcs-hash:20090202231709-f2e3e-e25ac616f7eba3781b16e3e2f2b04548504dca16.gz

--- ghc-6.8.2/libraries/base/Control/Concurrent/QSem.hs
+++ ghc-6.8.2/libraries/base/Control/Concurrent/QSem.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Control.Concurrent.QSem
--- Copyright   :  (c) The University of Glasgow 2001
+-- Copyright   :  (c) The University of Glasgow 2001, Kenny Graunke 2009
 -- License     :  BSD-style (see the file libraries/base/LICENSE)
 -- 
 -- Maintainer  :  libraries@haskell.org
@@ -20,58 +20,44 @@ module Control.Concurrent.QSem
 	  signalQSem	-- :: QSem -> IO ()
 	) where
 
-import Prelude
-import Control.Concurrent.MVar
-import Data.Typeable
+import Control.Monad(unless)
+import Data.IORef
+import LwConc.PTM
+import LwConc.Scheduler(getNextThread, schedule)
+import LwConc.Substrate(switch, SCont)
 
-#include "Typeable.h"
-
--- General semaphores are also implemented readily in terms of shared
--- @MVar@s, only have to catch the case when the semaphore is tried
--- waited on when it is empty (==0). Implement this in the same way as
--- shared variables are implemented - maintaining a list of @MVar@s
--- representing threads currently waiting. The counter is a shared
--- variable, ensuring the mutual exclusion on its access.
+-- QSems implemented atop PTM.
+-- These now wake up all waiting threads and let them compete for the resource.
+-- This lets the scheduler decide who gets it next, preventing a large source
+-- of priority inversion.  It may suffer some starvation.
 
 -- |A 'QSem' is a simple quantity semaphore, in which the available
 -- \"quantity\" is always dealt with in units of one.
-newtype QSem = QSem (MVar (Int, [MVar ()]))
-
-INSTANCE_TYPEABLE0(QSem,qSemTc,"QSem")
+newtype QSem = QSem (PVar (Int, [SCont]))
 
 -- |Build a new 'QSem'
 newQSem :: Int -> IO QSem
 newQSem init = do
-   sem <- newMVar (init,[])
+   sem <- newPVarIO (init,[])
    return (QSem sem)
 
 -- |Wait for a unit to become available
 waitQSem :: QSem -> IO ()
-waitQSem (QSem sem) = do
-   (avail,blocked) <- takeMVar sem  -- gain ex. access
-   if avail > 0 then
-     putMVar sem (avail-1,[])
-    else do
-     block <- newEmptyMVar
-      {-
-	Stuff the reader at the back of the queue,
-	so as to preserve waiting order. A signalling
-	process then only have to pick the MVar at the
-	front of the blocked list.
-
-	The version of waitQSem given in the paper could
-	lead to starvation.
-      -}
-     putMVar sem (0, blocked++[block])
-     takeMVar block
+waitQSem q@(QSem pv) =
+  do bucket <- newIORef False
+     switch $ \currThread -> do (avail,ts) <- readPVar pv
+                                if avail > 0
+                                   then do writePVar pv (avail-1,[])
+                                           unsafeIOToPTM $ writeIORef bucket True
+                                           return currThread
+                                   else do writePVar pv (avail, currThread:ts)
+                                           getNextThread
+     gotIt <- readIORef bucket
+     unless gotIt $ waitQSem q
 
 -- |Signal that a unit of the 'QSem' is available
 signalQSem :: QSem -> IO ()
-signalQSem (QSem sem) = do
-   (avail,blocked) <- takeMVar sem
-   case blocked of
-     [] -> putMVar sem (avail+1,[])
+signalQSem (QSem pv) = atomically $ do (avail,blocked) <- readPVar pv
+                                       writePVar pv (avail+1,[])
+                                       mapM_ schedule blocked -- wake 'em all; duke it out; let the schedule decide
 
-     (block:blocked') -> do
-	   putMVar sem (0,blocked')
-	   putMVar block ()
